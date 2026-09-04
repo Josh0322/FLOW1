8b78dda4852e097a6852349a0b7dd2b98dbcfbe7
josh aderes <joshpaderes2730@gmail.com>
2026-09-04 10:28:41 +0200

    SP1

---
+++ src/src/SP1.java
@@ -0,0 +1,32 @@
+public class SP1 {
+    public static void main(String[] args) {
+        String band = "The Static Waves";
+        String genre = "R";
+        int fans = 8500;
+        int maxFans = 15000;
+        int fameLevel = 2;
+        int xp = 3200;
+        int experiencePoints = 3200;
+        double money = 4750.50;
+        boolean isActive = true;
+        String genres = "‘R’ (Rock), ‘E’ (Electronic), ‘H’ (HipHop), ‘P’ (Pop)";
+        String[] names = {"Electric Dreams", "Midnight Run", "Broken Strings", "Neon Lights"};
+
+        System.out.println("=== BAND PROFILE ===");
+        System.out.println("Name: " + band);
+        System.out.println("Genre: " + genre);
+        System.out.println("Fame level: " + fameLevel);
+        System.out.println("Fans: " + fans + "/" + maxFans);
+        System.out.println("XP: " + xp);
+        System.out.println("Money: $" + money);
+        System.out.println("Active: " + isActive);
+
+        System.out.println("Repertoire (4 songs): ");
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println("- " + names[i]);
+
+
+        }
+    }
+}
